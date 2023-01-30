ce9a63f1d414fa27a1c785902feb88b04addc668
unknown <ayoubgd19@gmail.com>
2023-01-30 19:56:52 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import {  useState } from "react";
+import { useState } from "react";
 
 
 
 function App() {
   const [result, setRresult] = useState('');
-  const [users, setUsers] = useState([]);
-
-
-
 
   const handlClickBtn = (e) => {
     setRresult(result.concat(e.target.name))
@@ -29,33 +25,38 @@ function App() {
     }
   }
   return (
-    <div className="flex h-screen">
-      <div className="m-auto text-center p-10 w-[500px]">
-        <div className="mt-4">
-          <input type="text" value={result} />
-          <div className="buttons">
-            <button onClick={handlClickBtn} name='7'>7</button>
-            <button onClick={handlClickBtn} name='8'>8</button>
-            <button onClick={handlClickBtn} name='9'>9</button>
-            <button className="highlight" onClick={handleDelete}>Del</button>
-            <button onClick={handlClickBtn} name='4'>4</button>
-            <button onClick={handlClickBtn} name='5'>5</button>
-            <button onClick={handlClickBtn} name='6'>6</button>
-            <button className="highlight" onClick={handlClickBtn} name='+'>&#43;</button>
-            <button onClick={handlClickBtn} name='1'>1</button>
-            <button onClick={handlClickBtn} name='2'>2</button>
-            <button onClick={handlClickBtn} name='3'>3</button>
-            <button className="highlight" onClick={handlClickBtn} name='-'>&#45;</button>
-            <button className="highlight" onClick={handlClickBtn} name='.'>&#x60C;</button>
-            <button onClick={handlClickBtn} name='0'>0</button>
-            <button className="highlight" onClick={handlClickBtn} name='/'>&#x2f;</button>
-            <button className="highlight" onClick={handlClickBtn} name='*'>&times;</button>
-            <button className="col-span-2 highlight" onClick={handleReset}>Reset</button>
-            <button onClick={handleResult} className="col-span-2 highlight">=</button>
+    <>
+      <div className="flex h-screen">
+        <div className="m-auto text-center p-10 w-[500px]">
+          <div className="mt-4">
+            <input type="text" value={result} />
+            <div className="buttons">
+              <button onClick={handlClickBtn} name='7'>7</button>
+              <button onClick={handlClickBtn} name='8'>8</button>
+              <button onClick={handlClickBtn} name='9'>9</button>
+              <button className="highlight" onClick={handleDelete}>Del</button>
+              <button onClick={handlClickBtn} name='4'>4</button>
+              <button onClick={handlClickBtn} name='5'>5</button>
+              <button onClick={handlClickBtn} name='6'>6</button>
+              <button className="highlight" onClick={handlClickBtn} name='+'>&#43;</button>
+              <button onClick={handlClickBtn} name='1'>1</button>
+              <button onClick={handlClickBtn} name='2'>2</button>
+              <button onClick={handlClickBtn} name='3'>3</button>
+              <button className="highlight" onClick={handlClickBtn} name='-'>&#45;</button>
+              <button className="highlight" onClick={handlClickBtn} name='.'>&#x60C;</button>
+              <button onClick={handlClickBtn} name='0'>0</button>
+              <button className="highlight" onClick={handlClickBtn} name='/'>&#x2f;</button>
+              <button className="highlight" onClick={handlClickBtn} name='*'>&times;</button>
+              <button className="col-span-2 highlight" onClick={handleReset}>Reset</button>
+              <button onClick={handleResult} className="col-span-2 highlight">=</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <footer className="text-center bg-black p-3 text-white">
+        Creted by ❤️ And ☕ All Right Riserved &copy; <a href="https://ayoubguidi.com">Ayoub™</a>
+      </footer>
+    </>
   );
 }
 
