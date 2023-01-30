3f076654b434a232ab83b41b698fa5d8b4c43c27
unknown <ayoubgd19@gmail.com>
2023-01-30 17:09:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import {  useState } from "react";
+
+
 
 function App() {
+  const [result, setRresult] = useState('');
+  const [users, setUsers] = useState([]);
+
+
+
+
+  const handlClickBtn = (e) => {
+    setRresult(result.concat(e.target.name))
+  }
+
+  const handleReset = () => {
+    setRresult('');
+  }
+
+  const handleDelete = () => {
+    setRresult(result.slice(0, -1))
+  }
+
+  const handleResult = () => {
+    try {
+      setRresult(eval(result).toString())
+    } catch (error) {
+      setRresult('error')
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex h-screen">
+      <div className="m-auto text-center p-10 w-[500px]">
+        <div className="mt-4">
+          <input type="text" value={result} />
+          <div className="buttons">
+            <button onClick={handlClickBtn} name='7'>7</button>
+            <button onClick={handlClickBtn} name='8'>8</button>
+            <button onClick={handlClickBtn} name='9'>9</button>
+            <button className="highlight" onClick={handleDelete}>Del</button>
+            <button onClick={handlClickBtn} name='4'>4</button>
+            <button onClick={handlClickBtn} name='5'>5</button>
+            <button onClick={handlClickBtn} name='6'>6</button>
+            <button className="highlight" onClick={handlClickBtn} name='+'>&#43;</button>
+            <button onClick={handlClickBtn} name='1'>1</button>
+            <button onClick={handlClickBtn} name='2'>2</button>
+            <button onClick={handlClickBtn} name='3'>3</button>
+            <button className="highlight" onClick={handlClickBtn} name='-'>&#45;</button>
+            <button className="highlight" onClick={handlClickBtn} name='.'>&#x60C;</button>
+            <button onClick={handlClickBtn} name='0'>0</button>
+            <button className="highlight" onClick={handlClickBtn} name='/'>&#x2f;</button>
+            <button className="highlight" onClick={handlClickBtn} name='*'>&times;</button>
+            <button className="col-span-2 highlight" onClick={handleReset}>Reset</button>
+            <button onClick={handleResult} className="col-span-2 highlight">=</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
